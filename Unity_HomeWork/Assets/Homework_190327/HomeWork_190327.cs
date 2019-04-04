6077ad7988d46b3c1f0f91fef47479a5cd38691b
BIACKFICK <1623849744@qq.com>
2019-04-04 16:31:51 +0800

    合并Unity作业 27-28号

---
+++ Unity_HomeWork/Assets/Homework_190327/HomeWork_190327.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeWork_190327 : MonoBehaviour
+{
+    private GameObject playerPrefab;
+    private GameObject player;
+
+    void Start()
+    {
+        playerPrefab = Resources.Load<GameObject>("Player");
+        System.IO.File.Create(Application.persistentDataPath + "/" + "war4.sav");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += player.transform.forward * 10 * Time.deltaTime;
+        }
+    }
+}
